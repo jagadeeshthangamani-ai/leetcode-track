b0c82deccd5cf7e0904a6657907211355fac66c3
Jagadeeswaran p <jagadeeshthangamani@gmail.com>
2026-07-14 14:00:06 +0530

    Create 380-InsertDeleteGetrandomO1

---
+++ 380-InsertDeleteGetrandomO1/380-InsertDeleteGetrandomO1.java
@@ -0,0 +1,41 @@
+// Last updated: 7/14/2026, 2:00:03 PM
+class RandomizedSet {
+
+    private List<Integer> values;
+    private Map<Integer, Integer> valuesIdx;
+
+    public RandomizedSet() {
+        this.values = new ArrayList<>();
+        this.valuesIdx = new HashMap<>();
+    }
+
+    public boolean insert(int val) {
+        if (valuesIdx.containsKey(val)) {
+            return false;
+        }
+
+        valuesIdx.put(val, values.size());
+        values.add(val);
+
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if (!valuesIdx.containsKey(val)) {
+            return false;
+        }
+
+        int index = valuesIdx.get(val);
+        valuesIdx.put(values.get(values.size() - 1), index);
+        valuesIdx.remove(val);
+        values.set(index, values.get(values.size() - 1));
+        values.remove(values.size() - 1);
+
+        return true;
+    }
+
+    public int getRandom() {
+        int index = (int) (Math.random() * values.size());
+        return values.get(index);
+    }
+}
